3e77214e783058889860b94f9f175f696c58b06d
Caio <caio.102008@gmail.com>
2021-10-25 22:38:42 -0300

    Adicionando Hello World

---
+++ introducao/helloworld/src/main/java/br/com/caiohgp/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package br.com.caiohgp.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloWorld {
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+        return "Hello World!";
+    }
+}
